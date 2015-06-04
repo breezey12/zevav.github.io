d9a7135982e46353630bf0dbda5860883efa5c2c
Zev Averbach <zev@avtranscription.com>
2015-06-04 15:21:20 -0400

    added second function, this one introducing 'return' and multiple arguments

--- functions/word_measure_2.py
+++ functions/word_measure_2.py
@@ -3,4 +3,9 @@ def transform_and_measure_a_word(a_word):
     print "The only thing we have to fear is " + a_word + " itself."
     word_length = len(a_word)
     print a_word + " is " + str(word_length) + " letters long."
+
+user_word = raw_input("What word would you like to transform? ")
+transform_and_measure_a_word(user_word)
+another_word = "Lake Champlaign"
+transform_and_measure_a_word(another_word)
      
