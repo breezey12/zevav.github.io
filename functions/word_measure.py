43788a841934e5978ea599db34843ed5139e97f1
Zev Averbach <zev@avtranscription.com>
2015-06-04 14:04:02 -0400

    more content, plus added .ipython-in and .ipython-out classes for mimicking the ipython console

--- functions/word_measure.py
+++ functions/word_measure.py
@@ -1,10 +1,5 @@
-word_list = [] # make sure there's a list to append to
-# assign this word to a variable
-word_from_user = raw_input("Enter a word for me to measure. ")
-# assign the length of the word to another variable
-word_length = len(word_from_user)
-# append the word to the end of a list
-word_list.append(word_from_user)
-# and tell me the word's length in plain language
-print word_from_user + " is " + str(word_length) + " letters long."
-print word_list
+the_word = raw_input("What word would you like to play with? (make sure it has an 'a' in it) ")
+print the_word.lower().replace('a','x')
+print "The only thing we have to fear is " + the_word + " itself."
+word_length = len(the_word)
+print the_word + " is " + str(word_length) + " letters long."
---
+++ functions/word_measure_2.py
@@ -0,0 +1,6 @@
+def transform_and_measure_a_word(a_word):
+    print a_word.lower().replace('a','x')
+    print "The only thing we have to fear is " + a_word + " itself."
+    word_length = len(a_word)
+    print a_word + " is " + str(word_length) + " letters long."
+     
